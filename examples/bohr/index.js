c5620790e8ca753f5028d669834c63f9a23df9f0
Kevin Doughty <github@kxdx.org>
2016-12-18 16:17:37 -0500

    It's time to get hyper

---
+++ examples/bohr/index.js
@@ -0,0 +1,142 @@
+import { decorate } from "../../hyperact.js";
+
+function push(progress) {
+	var result = 1 + 0.5 * (1-progress) * Math.sin(progress * Math.PI * 2);
+	return Number(result).toFixed(4);
+}
+
+var shorter = 1;
+var longer = 10;
+
+function Hydrogen(kdMeson,electron) {
+	decorate(this);
+	
+	this.kdMeson = kdMeson;
+	this.electron = electron;
+	this.shiftKeyPressed = false;
+	this.radius = 200;
+	this.value = 0;
+	this.outer = 0;
+	this.compensate = 0;
+	this.x = 0;
+	this.y = 0;
+	
+	this.registerAnimatableProperty("x");
+	this.registerAnimatableProperty("y");
+	this.registerAnimatableProperty("outer");
+	this.registerAnimatableProperty("value");
+	this.registerAnimatableProperty("compensate");
+}
+
+Hydrogen.prototype.layout = function() {
+	var width = document.body.offsetWidth;
+	var height = document.body.offsetHeight;
+	var centerX = width/2;
+	var centerY = height/2;
+	
+	var presentation = this.presentation;
+	var presentationValue = presentation.value;
+	
+	var amount = Math.random() * Math.PI * 2;
+	var destinationValue = (amount) - Math.PI;
+	
+	var ratio = shorter/longer;
+	var travelled = ratio * Math.PI * 2;
+	this.destinationX = Math.cos(destinationValue + travelled) * this.radius + centerX;
+	this.destinationY = Math.sin(destinationValue + travelled) * this.radius + centerY;
+
+	var oldX = Math.cos(presentationValue) * this.radius + centerX;
+	var oldY = Math.sin(presentationValue) * this.radius + centerY;
+	var newX = Math.cos(destinationValue) * this.radius + centerX;
+	var newY = Math.sin(destinationValue) * this.radius + centerY;
+	
+	this.oldX = oldX;
+	this.oldY = oldY;
+	this.newX = newX;
+	this.newY = newY;
+	
+	var duration = shorter * (this.shiftKeyPressed ? 5 : 1);
+	
+	var compensate = presentation.compensate;
+	this.outer = destinationValue - compensate;
+	
+	this.addAnimation({
+		property: "x",
+		duration: duration,
+		from: oldX,
+		to: newX
+	});
+	this.addAnimation({
+		property: "y",
+		duration: duration,
+		from: oldY,
+		to: newY
+	});
+	this.addAnimation({
+		property:"value",
+		duration: longer,
+		from:destinationValue,
+		to:destinationValue + Math.PI * 2,
+		iterations:Infinity,
+		easing:"linear",
+		blend:"absolute"
+	},"revolution");
+};
+
+Hydrogen.prototype.animationForKey = function(key,value) {
+	return shorter * (this.shiftKeyPressed ? 5 : 1);
+}
+
+Hydrogen.prototype.toggleKeys = function(e) {
+	this.shiftKeyPressed = e.shiftKey;
+}
+
+Hydrogen.prototype.display = function() {
+	var width = document.body.offsetWidth;
+	var height = document.body.offsetHeight;
+	var centerX = width/2;
+	var centerY = height/2;
+	
+	if (this.kdMeson) {
+		var x = this.x + Math.cos(this.value) * this.radius + centerX; // both x and value are interpolated
+		var y = this.y + Math.sin(this.value) * this.radius + centerY; // both y and value are interpolated
+		var innerString = "translate3d("+x+"px, "+y+"px, 0)";
+		this.kdMeson.style.webkitTransform = innerString;
+		this.kdMeson.style.transform = innerString;
+	}
+	if (this.electron) {
+		var outerX = Math.cos(this.outer + this.compensate) * this.radius + centerX; // outer is interpolated
+		var outerY = Math.sin(this.outer + this.compensate) * this.radius + centerY; // outer is interpolated
+		var outerString = "translate3d("+outerX+"px, "+outerY+"px, 0)";
+		this.electron.style.webkitTransform = outerString;
+		this.electron.style.transform = outerString;
+	}
+}
+
+var kdMeson = document.getElementById("kdMeson");
+var electron = document.getElementById("electron");
+
+var hydrogen = new Hydrogen(kdMeson,electron);
+
+hydrogen.addAnimation({
+	property:"value",
+	duration: longer,
+	from:0,
+	to:Math.PI * 2,
+	iterations:Infinity,
+	easing:"linear",
+	blend:"absolute"
+},"revolution");
+
+hydrogen.addAnimation({
+	property:"compensate",
+	duration: longer,
+	from:-Math.PI * 2,
+	to:0,
+	iterations:Infinity,
+	easing:"linear"
+},"compensate");
+
+document.addEventListener("mousedown", hydrogen.layout.bind(hydrogen));
+document.addEventListener('keydown',hydrogen.toggleKeys.bind(hydrogen));
+document.addEventListener('keyup',hydrogen.toggleKeys.bind(hydrogen));
---
+++ examples/rococo/index.js
@@ -0,0 +1,152 @@
+import { decorate, disableAnimation, HyperArray, HyperNumber } from "../../hyperact.js";
+
+function elastic(progress,omega,zeta) {
+	const beta = Math.sqrt(1.0 - zeta * zeta);
+	const value = 1.0 / beta * Math.exp(-zeta * omega * progress) * Math.sin(beta * omega * progress + Math.atan(beta / zeta));
+	return 1-value;
+};
+
+function easing(progress) {
+	progress = 1 - Math.cos( progress * Math.PI / 2 );
+	return elastic(progress, 15, 0.6);
+}
+
+const duration = 1.0;
+
+class View {
+	constructor(element) {
+		this.element = element;
+		this.iterations = 50;
+		this.vertices = 1000;
+		this.radius = 75;
+		this.value = 0;
+		this.positionArray = this.plot();
+		
+		decorate(this);
+		this.registerAnimatableProperty("positionArray");
+	}
+	animationForKey(key,value,previous) {
+		if (key === "positionArray") return {
+			type: new HyperArray(HyperNumber, this.vertices * 2),
+			duration: duration,
+			easing: easing
+		}
+	}
+	plot() {
+		const canvas = this.element;
+		const width = canvas.width;
+		const height = canvas.height;
+		const centerX = width/2;
+		const centerY = height/2;
+		const phi = this.value * Math.PI;
+		const array = [];
+		const slice = (Math.PI * 2) / this.vertices;
+		let j = this.vertices;
+		let theta = 0;
+		while (--j) { // array length is not vertices * 2 !!!
+			theta -= slice;
+			let i = this.iterations;
+			let x = centerX;
+			let y = centerY;
+			do {
+				const value = (i * theta) + (i * i * phi);
+				x += ( Math.sin(value) / i ) * this.radius;
+				y += ( Math.cos(value) / i ) * this.radius;
+			} while (--i);
+			array.push(x);
+			array.push(y);
+		}
+		return array;
+	}
+	layout(value) {
+		this.value = value;
+		this.positionArray = this.plot();
+	}
+	display() {
+		const canvas = this.element;
+		const context = canvas.getContext("2d");
+		const width = canvas.width;
+		const height = canvas.height;
+		context.clearRect(0, 0, width, height);
+		const array = this.positionArray;
+		const length = array.length;
+		if (length > 1) {
+			context.beginPath();
+			context.moveTo(array[0],array[1]);
+			for (let i = 2; i<length; i+=2) {
+				context.lineTo(array[i],array[i+1]);
+			}
+			context.closePath();
+			context.stroke();
+		}
+	}
+}
+
+class Slider {
+	constructor(element) {
+		decorate(this);
+		this.element = element;
+		this.value = 0;
+		this.registerAnimatableProperty("value");
+	}
+	animationForKey(key,value,previous) {
+		return {
+			duration: duration,
+			easing: easing
+		}
+	}
+	layout(value) {
+		this.value = value;
+	}
+	display() {
+		this.element.value = this.value * 1000;
+	}
+}
+
+let rococo = Math.random();
+
+const input = document.createElement("input");
+input.type = "range";
+input.min = 0;
+input.max = 1000;
+input.value = rococo * 1000;
+document.body.appendChild(input);
+const slider = new Slider(input);
+
+const canvas = document.createElement("canvas");
+document.body.appendChild(canvas);
+const view = new View(canvas);
+
+function resize() {
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	disableAnimation(true);
+	view.layout(rococo);
+	slider.layout(rococo);
+};
+resize();
+
+window.addEventListener("resize", resize);
+canvas.addEventListener("mousedown", (e) => {
+	e.stopPropagation();
+	e.preventDefault();
+	rococo = Math.random();
+	view.layout(rococo);
+	slider.layout(rococo);
+});
+document.addEventListener("mouseup", (e) => {
+	input.blur();
+});
+input.addEventListener("input", (e) => {
+	rococo = input.value / 1000;
+	view.layout(rococo);
+	slider.layout(rococo);
+});
+input.addEventListener("change", (e) => {
+	rococo = input.value / 1000;
+	view.layout(rococo);
+	slider.layout(rococo);
+})
+input.addEventListener("mousedown", (e) => {
+	input.focus();
+});
