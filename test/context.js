0dd9fa1d31a7a43cf994dcec638b6d0c4f4a8fd5
Kevin Doughty <github@kxdx.org>
2016-12-22 16:40:17 -0500

    added file

---
+++ test/context.js
@@ -0,0 +1,116 @@
+import { HyperContext, HyperTransaction } from "../source/context.js";
+var assert = require("assert");
+
+function isFunction(w) {
+	return w && {}.toString.call(w) === "[object Function]";
+}
+
+class Target {
+	presentation() {
+		return {
+			presentation:true
+		};
+	}
+}
+
+
+
+
+describe("context", function() {
+	describe("zero", function() {
+		it("function", function() {
+			assert(isFunction(function() {}));
+			assert(!isFunction({}));
+			assert(!isFunction("[object Function]"));
+		});
+		it("HyperContext", function() {
+			assert(isFunction(HyperContext));
+		});
+		it("HyperTransaction", function() {
+			assert(isFunction(HyperTransaction));
+		});
+	});
+
+	describe("one", function() {
+
+		var context;
+
+		beforeEach( function() {
+			context = new HyperContext();
+		});
+		it("implicit outer transaction", function() {
+			const transaction = context.currentTransaction();
+			assert(transaction);
+		});
+		it("_automaticallyCommit is not exposed", function() {
+			const transaction = context.currentTransaction();
+			assert(transaction && !transaction._automaticallyCommit);
+		});
+		it("transaction time", function() {
+			const transaction = context.currentTransaction();
+			assert(transaction && (transaction.time || transaction.time === 0));
+		});
+		it("transactions can be nested", function() {
+			const one = context.currentTransaction();
+			const two = context.beginTransaction();
+			const three = context.beginTransaction();
+			assert(one && two && three && one.time === two.time && one.time === three.time);
+			context.commitTransaction();
+			context.commitTransaction();
+		});
+	});
+	describe("two", function() { // testing private implementation details
+		let context;
+		let target;
+		beforeEach( function() {
+			context = new HyperContext();
+			target = new Target();
+		});
+		it("registers and ticks", function(done) {
+			target.animationCount = 0;
+			const display = () => {
+				done();
+			};
+			const cleanup = function() {};
+			context.registerTarget(target, display, cleanup);
+		});
+		it("cleanup to remove animations", function(done) {
+			target.animationCount = 10;
+			const display = () => {};
+			const cleanup = () => {
+				target.animationCount--;
+				if (!target.animationCount) done();
+			};
+			context.registerTarget(target, display, cleanup);
+		});
+		it("two targets registered", function() {
+			target.animationCount = 0;
+			const display = () => {};
+			const cleanup = () => {};
+			context.registerTarget(target, display, cleanup);
+			
+			const secondTarget = new Target();
+			secondTarget.animationCount = 0;
+			context.registerTarget(secondTarget, display, cleanup);
+			assert(context.targets && context.targets.length === 2);
+		});
+		it("targets are deregistered", function(done) {
+			target.animationCount = 10;
+			const display = () => {};
+			const cleanup = () => {
+				target.animationCount--;
+			};
+			context.registerTarget(target, display, cleanup);
+			const secondTarget = new Target();
+			secondTarget.animationCount = target.animationCount + 1;
+			const secondDisplay = () => {
+				if (context.targets.length === 1 && context.targets[0] === secondTarget) done();
+			};
+			const secondCleanup = () => {
+				secondTarget.animationCount--;
+			};
+			context.registerTarget(secondTarget, secondDisplay, secondCleanup);
+		});
+
+	});
+});
