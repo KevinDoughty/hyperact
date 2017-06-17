c9b5c239d9d88aff1eb60e8bb3a7cb374b99fdd4
Kevin Doughty <github@kxdx.org>
2017-06-17 08:51:27 -0400

    removed everything related to webpack chunks

--- webpack.config.js
+++ webpack.config.js
@@ -21,25 +21,7 @@ if (process.env.WEBPACK_ENV === "build") modulesPlugins = [
 		sourceMap: false
 	})
 ];
-var chunksPlugins = [
-	new webpack.optimize.CommonsChunkPlugin({
-// 		filename: "hyperact.js",
-// 		name: "hyperact",
-// 		chunks: ["core","types"]
-		filename: "core.js",
-		name: "core",
-		chunks: ["types"]
-	})
-];
-if (process.env.WEBPACK_ENV === "build") {
-	chunksPlugins.push(new webpack.optimize.UglifyJsPlugin({
-		compress: true,
-		mangle: true,
-		beautify: false,
-		comments: false,
-		sourceMap: false
-	}));
-}
+
 module.exports = [
 	{
 		entry: "./source/hyperact.js",
@@ -60,31 +42,5 @@ module.exports = [
 			]
 		},
 		plugins: modulesPlugins
-	},
-	{ // chunks deprecated:
-		entry: {
-			core: "./source/core.js",
-			types: "./source/types.js",
-			style: "./source/style/style.js"
-		},
-		output: {
-			path: path.join(__dirname, "chunks"),
-			filename: "[name].js",
-			library: ["Hyperact","[name]"],
-			libraryTarget: "umd"
-		},
-		module: {
-			loaders: [
-				{
-					test: /\.js$/,
-					loader: "babel-loader",
-					exclude: /node_modules/,
-					query: {
-						presets: ["es2015"]
-					}
-				}
-			]
-		},
-		plugins: chunksPlugins
 	}
 ];
